158a6d8887adfd1acebf6ddec321c52702081618
Nick.Haer <nhaer123@gmail.com>
2019-12-12 23:09:10 -0600

    made navbar act more predictably on page scroll

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 window.onscroll = () => scrollCheck();
 
-
-
 function scrollCheck () {
-    console.log("muppet")
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+
+    console.log(document.documentElement.scrollTop)
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         document.querySelector("#navbar").style.padding = ".5%";
-} else {
-    document.querySelector("#navbar").style.padding = "3%";
+} else if (document.documentElement.scrollTop + 30 < 100){
+    document.querySelector("#navbar").style.padding = "1.5%";
 }
 }
+
+// let paused = false;
+
+// const stars = document.querySelector("#jumbotron")
+
+// stars.addEventListener("click", toggleVideo)
+
+// // .addEventListener("click", () => console.log("muppet"))
+// function toggleVideo () {
+//     if (paused) {
+//         stars.firstChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.play();
+//         paused = false;
+//     } else {
+//         stars.firstChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.pause()
+//         paused = true;
+//     }
+
+// }
