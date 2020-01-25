2e2588fcee338fb3c3402f1d2aac0646dfe22c0c
Nick.Haer <nhaer123@gmail.com>
2020-01-24 20:37:12 -0600

    changed nav bar to respond to grid, step 1 of making mobile responsive

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 window.onscroll = () => scrollCheck();
 
-function scrollCheck () {
-
-    console.log(document.documentElement.scrollTop)
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.querySelector("#navbar").style.padding = ".5%";
-} else if (document.documentElement.scrollTop + 30 < 100){
-    document.querySelector("#navbar").style.padding = "1.5%";
-}
+function scrollCheck() {
+  // console.log(document.documentElement.scrollTop);
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    document.querySelector('.grid-container').style.padding = '.5%';
+  } else if (document.documentElement.scrollTop + 30 < 100) {
+    document.querySelector('.grid-container').style.padding = '1.5%';
+  }
 }
